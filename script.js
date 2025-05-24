4ceb89d1060165c7a00c9ec0397550653a0c5d6e
Melanie <183251709+mel723@users.noreply.github.com>
2025-05-24 21:06:40 +1000

    Working calculator

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+let numbers = [];
+let operator = "";
+let result;
+
+const display = document.querySelector("#number-display");
+const numberPad = document.querySelector(".number-pad");
+
+let number = 0;
+
+for (let i = 0; i < 4; i++) {
+  const row = document.createElement("div");
+  row.classList.add("row");
+
+  for (let j = 0; j < 3; j++) {
+    if (number > 9) {
+      break;
+    }
+
+    const button = document.createElement("button");
+    button.textContent = number;
+    button.addEventListener("click", (e) => {
+      numbers.push(Number(e.target.textContent));
+      display.textContent = e.target.textContent;
+    });
+    row.appendChild(button);
+    number++;
+  }
+
+  numberPad.appendChild(row);
+}
+
+function setOperator(character) {
+  operator = character;
+}
+
 function add(a, b) {
   return a + b;
 }
@@ -14,15 +49,27 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(operator, num1, num2) {
+function operate() {
+  const num1 = numbers[0];
+  const num2 = numbers[1];
+
   switch (operator) {
     case "+":
-      return add(num1, num2);
+      result = add(num1, num2);
+      break;
     case "-":
-      return subtract(num1, num2);
+      result = subtract(num1, num2);
+      break;
     case "*":
-      return multiply(num1, num2);
+      result = multiply(num1, num2);
+      break;
     case "/":
-      return divide(num1, num2);
+      result = divide(num1, num2);
+      break;
   }
+  
+  numbers.splice(0, numbers.length);
+  numbers.push(result);
+
+  display.textContent = result;
 }
